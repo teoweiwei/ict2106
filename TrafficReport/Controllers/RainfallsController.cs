1ea637158dc3a1bf16dc825f2c0e184b1a3655af
teoweiwei <teoweisquare@outlook.com>
2016-03-06 17:05:26 +0800

    Added code from previous prototype

--- TrafficReport/Controllers/RainfallsController.cs
+++ TrafficReport/Controllers/RainfallsController.cs
@@ -6,127 +6,45 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using TrafficReport.DAL;
 using TrafficReport.Models;
 
 namespace TrafficReport.Controllers
 {
     public class RainfallsController : Controller
     {
-        private trafficreportEntities db = new trafficreportEntities();
+        private RainfallGateway rainfallGateway = new RainfallGateway();
 
         // GET: Rainfalls
         public ActionResult Index()
         {
-            var tblRainfalls = db.tblRainfalls.Include(t => t.tblLocationName);
-            return View(tblRainfalls.ToList());
-        }
-
-        // GET: Rainfalls/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tblRainfall tblRainfall = db.tblRainfalls.Find(id);
-            if (tblRainfall == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tblRainfall);
-        }
-
-        // GET: Rainfalls/Create
-        public ActionResult Create()
-        {
-            ViewBag.rfLocation = new SelectList(db.tblLocationNames, "lnID", "lnLocationName");
             return View();
         }
 
-        // POST: Rainfalls/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        public ActionResult Upload()
+        {
+            return View(rainfallGateway.SelectAll());
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "rfID,rfDate,rfLocation,rfValue")] tblRainfall tblRainfall)
+        public ActionResult Upload(HttpPostedFileBase upload)
         {
             if (ModelState.IsValid)
             {
-                db.tblRainfalls.Add(tblRainfall);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if(!rainfallGateway.Upload(upload))
+                {
+                    ModelState.AddModelError("File", "Error in uploading file");
+                    return View("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("File", "Error in uploading file");
+                return View("Index");
             }
 
-            ViewBag.rfLocation = new SelectList(db.tblLocationNames, "lnID", "lnLocationName", tblRainfall.rfLocation);
-            return View(tblRainfall);
-        }
-
-        // GET: Rainfalls/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tblRainfall tblRainfall = db.tblRainfalls.Find(id);
-            if (tblRainfall == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.rfLocation = new SelectList(db.tblLocationNames, "lnID", "lnLocationName", tblRainfall.rfLocation);
-            return View(tblRainfall);
-        }
-
-        // POST: Rainfalls/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "rfID,rfDate,rfLocation,rfValue")] tblRainfall tblRainfall)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(tblRainfall).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.rfLocation = new SelectList(db.tblLocationNames, "lnID", "lnLocationName", tblRainfall.rfLocation);
-            return View(tblRainfall);
-        }
-
-        // GET: Rainfalls/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tblRainfall tblRainfall = db.tblRainfalls.Find(id);
-            if (tblRainfall == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tblRainfall);
-        }
-
-        // POST: Rainfalls/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            tblRainfall tblRainfall = db.tblRainfalls.Find(id);
-            db.tblRainfalls.Remove(tblRainfall);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return View(rainfallGateway.SelectAll());
         }
     }
 }
---
+++ TrafficReport/DAL/RainfallGateway.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Globalization;
+using TrafficReport.Models;
+
+namespace TrafficReport.DAL
+{
+    public class RainfallGateway : DataGateway<tblRainfall>
+    {
+        public Boolean Upload(HttpPostedFileBase upload)
+        {
+            if (upload != null && upload.ContentLength > 0)
+            {
+                if (upload.FileName.EndsWith(".csv"))
+                {
+                    Stream stream = upload.InputStream;
+                    StreamReader read = new StreamReader(stream);
+
+                    List<string> row = new List<string>();
+                    tblRainfall rainfallData = new tblRainfall();
+                    read.ReadLine();
+                    
+                    while (!read.EndOfStream)
+                    {
+                        row = read.ReadLine().Split(',').ToList();
+
+                        string locationName = row[0];
+                        int locationId = db.tblLocationNames.Where(l => l.lnLocationName.Equals(locationName)).ToList()[0].lnID;
+
+                        rainfallData.rfDate = DateTime.ParseExact(row[3] + "/" + row[2] + "/" + row[1], "d/M/yyyy", CultureInfo.InvariantCulture);
+                        rainfallData.rfLocation = locationId;
+                        rainfallData.rfValue = decimal.Parse(row[4]);
+
+                        //Add record into DB
+                        Insert(rainfallData);
+                    }
+
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
