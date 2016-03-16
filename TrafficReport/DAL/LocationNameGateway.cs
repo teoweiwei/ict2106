b103cf9173f6545109c399729fd1ed5ca3a6619c
teoweiwei <teoweisquare@outlook.com>
2016-03-16 22:52:54 +0800

    Remove unnecessary files

--- TrafficReport/DAL/LocationNameGateway.cs
+++ TrafficReport/DAL/LocationNameGateway.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using TrafficReport.Models;
 
 namespace TrafficReport.DAL
 {
     public class LocationNameGateway : DataGateway<tblLocationName>
     {
-        
-
     }
 }
