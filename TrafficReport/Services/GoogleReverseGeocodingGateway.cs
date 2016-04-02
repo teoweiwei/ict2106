b8eb25b923987ed32f59b27c6157fe086180d13d
teoweiwei <teoweisquare@outlook.com>
2016-04-02 23:31:10 +0800

    Added more comments

--- TrafficReport/Services/GoogleReverseGeocodingGateway.cs
+++ TrafficReport/Services/GoogleReverseGeocodingGateway.cs
@@ -7,17 +7,20 @@ using TrafficReport.Models;
 
 namespace TrafficReport.Services
 {
+    //Service gateway to access Google Geocoding web service
     public class GoogleReverseGeocodingGateway
     {
         private const string URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
         private const string API_KEY = "AIzaSyC7f9VGd6gfZaJO3IAHZl05sHhFTl2V5UM";
 
+        //Get road name based on the given latitude and longitude coordinates
         public string GetRoadNameGeolocation(double? lat, double? lng)
         {
             try
             {
                 string jsonData;
 
+                //Setting up HTTP request message
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL+lat.ToString() + "," + lng.ToString() + "&result_type=route&key=" + API_KEY);
                 request.Accept = "application/json";
                 request.Host = "maps.googleapis.com";
@@ -25,6 +28,7 @@ namespace TrafficReport.Services
                 request.ContentLength = 0;
                 request.Method = "GET";
 
+                //Reponse from web service
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 using (Stream responseStream = response.GetResponseStream())
                 {
@@ -32,8 +36,10 @@ namespace TrafficReport.Services
                     jsonData = reader.ReadToEnd();
                 }
 
+                //Convert JSON data into a data model
                 GoogleReverseGeocodingModel.GeocodeingData geocodingData = JsonConvert.DeserializeObject<GoogleReverseGeocodingModel.GeocodeingData>(jsonData);
 
+                //Return data model when result is successfully received
                 if(geocodingData.status.Equals("OK"))
                 {
                     return geocodingData.results[0].address_components[0].long_name;
--- TrafficReport/Services/LTADataMallGateway.cs
+++ TrafficReport/Services/LTADataMallGateway.cs
@@ -9,6 +9,7 @@ using TrafficReport.Models;
 
 namespace TrafficReport.Services
 {
+    //Service gateway to access LTADataMall web service
     public class LTADataMallGateway : ILTADataMallGateway
     {
         private const string ACCIDENT_URL = "http://datamall.mytransport.sg/ltaodataservice.svc/IncidentSet";
@@ -16,22 +17,29 @@ namespace TrafficReport.Services
         private const string ACCOUNT_KEY = "73yoKcEiJDZCbxL3KOxLJw==";
         private const string UNIQUE_USER_ID = "4eab90a3-02d5-4362-8dc5-0cc5f32325b0";
 
+        //Request web service for traffic accident data
         public List<LTADataMallModel.AccidentData> GetLTAAccidentData()
         {
+            //Convert JSON data into a data model list
             LTADataMallModel.LTADataMallAccidentData ltaDataMallAccidentData = JsonConvert.DeserializeObject<LTADataMallModel.LTADataMallAccidentData>(LTADataMallRequest(ACCIDENT_URL));
 
+            //Return data model list
             return ltaDataMallAccidentData.d;
         }
 
+        //Request web service for traffic speed and road name data
         public List<LTADataMallModel.SpeedData> GetLTASpeedData()
         {
             List<LTADataMallModel.SpeedData> speedData = new List<LTADataMallModel.SpeedData>();
             int skipCount = 0;
 
+            //Convert JSON data into a data model list
             LTADataMallModel.LTADataMallSpeedBandData ltaDataMallSpeedBandData = JsonConvert.DeserializeObject<LTADataMallModel.LTADataMallSpeedBandData>(LTADataMallRequest(SPEED_URL + "?$skip=" + skipCount.ToString()));
 
+            //Continuously request for data until no data is receive
             do
             {
+                //Append new data into model
                 for (int i = 0; i < ltaDataMallSpeedBandData.d.Count(); i++)
                 {
                     speedData.Add(ltaDataMallSpeedBandData.d[i]);
@@ -42,15 +50,18 @@ namespace TrafficReport.Services
 
             } while (ltaDataMallSpeedBandData.d.Count != 0);
 
+            //Return data model
             return speedData;
         }
 
+        //Actual web service request procedure
         private string LTADataMallRequest(string url)
         {
             try
             {
                 string jsonData;
 
+                //Setting up HTTP request message
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 request.Headers.Add("AccountKey", ACCOUNT_KEY);
                 request.Headers.Add("UniqueUserID", UNIQUE_USER_ID);
@@ -59,6 +70,7 @@ namespace TrafficReport.Services
                 request.ContentLength = 0;
                 request.Method = "GET";
 
+                //Reponse from web service
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 using (Stream responseStream = response.GetResponseStream())
                 {
@@ -66,6 +78,7 @@ namespace TrafficReport.Services
                     jsonData = reader.ReadToEnd();
                 }
 
+                //Return JSON data
                 return jsonData;
             }
             catch (Exception e)
